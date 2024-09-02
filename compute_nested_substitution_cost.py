a6b7f4845284bc49462b00db478fecf6157ae87d
DVillanova <davidvillanovaaparisi@gmail.com>
2024-09-02 12:02:53 +0300

    Initial version of the method

---
+++ compute_nested_substitution_cost.py
@@ -0,0 +1,311 @@
+from zss import simple_distance, Node
+import copy
+
+CATEGORY = 0
+CHILDREN = 1
+MARK = 2
+
+#Given the representation of a Named Entity as a tuple (category, children) where children is a list that may contain
+#either strings (leaf nodes, transcriptions) or tuples (category, children) that represent subtrees, do a breadth-first
+#traversal of the tree to build the same structure in zss Nodes
+def build_tagging_tree(named_entity: list) -> Node:
+    if len(named_entity) == 0:
+        return None
+    
+    tagging_tree = Node(named_entity[CATEGORY])
+
+    #The elements in the queue have the path to follow from the root to find the parent node and the subtree
+    queue_nodes_to_visit_current_depth = [([], subtree) for subtree in named_entity[CHILDREN] if isinstance(subtree, list)]
+    queue_nodes_to_visit_next_depth = []
+    current_depth_index = 0
+    while len(queue_nodes_to_visit_current_depth) > 0:
+        (path_from_root, node_tuple) = queue_nodes_to_visit_current_depth.pop(0)
+        #Locate where to insert category of node_tuple in tagging_tree
+        parent_node = tagging_tree
+        for index_path in path_from_root:
+            parent_node = Node.get_children(parent_node)[index_path]
+        
+        #Insert the node
+        parent_node.addkid(Node(node_tuple[CATEGORY]))
+
+        #Add the children (next depth) to visit
+        path_from_root.append(current_depth_index)
+        for subtree in node_tuple[CHILDREN]: 
+            if isinstance(subtree, list):
+                queue_nodes_to_visit_next_depth.append((path_from_root, subtree))
+
+        #Update counter of index current depth
+        current_depth_index += 1
+
+        #If current_depth is empty, swap lists and reset index counter
+        if len(queue_nodes_to_visit_current_depth) == 0:
+            aux = queue_nodes_to_visit_current_depth
+            queue_nodes_to_visit_current_depth = queue_nodes_to_visit_next_depth
+            queue_nodes_to_visit_next_depth = aux
+
+            current_depth_index = 0
+
+    return tagging_tree
+
+#Return post-order traversal of non-leaf nodes (category nodes) from original representation (list)
+def post_order_traversal_named_entity(orig_named_entity: list, path_from_root=[], index_node=0) -> list:
+    if len(orig_named_entity) > 1: #Category node
+        named_entity = copy.deepcopy(orig_named_entity)
+
+        path_from_root = copy.deepcopy(path_from_root)
+        path_from_root.append((named_entity[CATEGORY], index_node))
+        post_order = []
+
+        children = named_entity[CHILDREN]
+        for idx, child in enumerate(children):
+            if isinstance(child, list):
+                post_order.extend(post_order_traversal_named_entity(child, path_from_root, idx))
+        
+        post_order.append(path_from_root)
+        return post_order
+    else:
+        return []
+
+
+def post_order_traversal_tagging_tree(tagging_tree: Node, path_from_root = [], index_node = 0) -> list:
+    if tagging_tree is not None:
+        path_from_root = path_from_root[:]
+        path_from_root.append((Node.get_label(tagging_tree), index_node))
+        post_order = []
+        children = list(Node.get_children(tagging_tree))
+
+        for idx,child in enumerate(children):
+            post_order.extend(post_order_traversal_tagging_tree(child, path_from_root, idx))
+        
+        post_order.append(path_from_root)
+        return post_order
+    else:
+        return []
+
+def obtain_list_tagged_transcriptions(named_entity: list, counter_dict: dict) -> list:
+    transcription_list = []
+    counter_tag = counter_dict.get(named_entity[CATEGORY], 0)
+    counter_dict[named_entity[CATEGORY]] = counter_tag + 1
+
+    for child in named_entity[CHILDREN]:
+        if isinstance(child, str): #Child is a transcription
+            transcription_list.append((named_entity[CATEGORY] + "_" + str(counter_tag), child))
+        else: #Child is a subtree
+            transcription_list.extend(obtain_list_tagged_transcriptions(child, counter_dict))
+
+    return transcription_list
+
+def add_marks_to_named_entity(named_entity: list, marks: list) -> list:
+    named_entity_copy = copy.deepcopy(named_entity)
+    post_order_traversal = post_order_traversal_named_entity(named_entity_copy)
+    
+    #Use marks information (in post-order) to add the mark to each node in the named_entity
+    for idx,path_from_root in enumerate(post_order_traversal):
+        mark = marks[idx]
+
+        node_to_modify = named_entity_copy
+        for node_step in path_from_root[1:]:
+            node_to_modify = node_to_modify[CHILDREN][node_step[1]]
+        
+        node_to_modify.append(mark)
+    
+    return named_entity_copy
+
+def obtain_marked_transcriptions(marked_named_entity: list) -> list:
+    transcription_list = []
+    for child in marked_named_entity[CHILDREN]:
+        if isinstance(child, str): #Child is a transcription
+            transcription_list.append((marked_named_entity[MARK], child))
+        else: #Child is a subtree
+            transcription_list.extend(obtain_marked_transcriptions(child))
+
+    return transcription_list
+
+
+#TODO: Make the method compute tagging error rate from tagging tree edit distance
+def obtain_tagged_and_marked_transcriptions(orig_ref_named_entity: list, orig_hyp_named_entity: list) -> tuple:
+    #Cases where one NE is empty => mark all the elements in the other as "wrong"
+    if len(orig_ref_named_entity) == 0:
+        tagged_and_marked_ref_transc = []      
+        
+        hyp_ne = copy.deepcopy(orig_hyp_named_entity)
+        tagged_hyp_transc = obtain_list_tagged_transcriptions(hyp_ne, dict())
+        tagged_and_marked_hyp_transc = []
+        for idx, tagged_item in enumerate(tagged_hyp_transc):
+            tagged_and_marked_hyp_transc.append((True, tagged_item[1], tagged_item[0]))
+
+        return (tagged_and_marked_ref_transc, tagged_and_marked_hyp_transc)
+
+    elif len(orig_hyp_named_entity) == 0:
+        tagged_and_marked_hyp_transc =  []
+
+        ref_ne = copy.deepcopy(orig_ref_named_entity)
+        tagged_ref_transc = obtain_list_tagged_transcriptions(ref_ne, dict())
+        tagged_and_marked_ref_transc = []
+        for idx, tagged_item in enumerate(tagged_ref_transc):
+            tagged_and_marked_ref_transc.append((True, tagged_item[1], tagged_item[0]))
+
+        return (tagged_and_marked_ref_transc, tagged_and_marked_hyp_transc)
+
+    #Construct Node object to represent tagging structure for zss computation of Tree Edit Distance
+    hyp_ne = copy.deepcopy(orig_hyp_named_entity)
+    ref_ne = copy.deepcopy(orig_ref_named_entity)
+    ref_tagging_tree = build_tagging_tree(ref_ne) 
+    hyp_tagging_tree = build_tagging_tree(hyp_ne)
+
+    
+
+    #Compute tree edit distance between reference and hypothesis tagging tree
+    #to employ the best sequence of edit operations in a post-process
+    (cost, operations) = simple_distance(ref_tagging_tree, 
+                                         hyp_tagging_tree,
+                                         label_dist=lambda a, b: int(a != b),
+                                         return_operations=True)
+
+    
+    #Operations are given in post-order (left to right, depth first) over reference and
+    #when inserting, the order of insertions is also given in post-order.
+    post_order_ref = post_order_traversal_named_entity(ref_ne)
+    marks_ref = [False]*len(post_order_ref)
+    idx_traversal_ref = 0
+
+    post_order_hyp = post_order_traversal_named_entity(hyp_ne)
+    marks_hyp = [False]*len(post_order_hyp)
+    idx_traversal_hyp = 0
+
+    orig_ref_ne = copy.deepcopy(ref_ne)
+    orig_hyp_ne = copy.deepcopy(hyp_ne)
+
+    for (idx,op) in enumerate(operations): 
+        path_node_ref = post_order_ref[idx_traversal_ref]
+        path_node_hyp = post_order_hyp[idx_traversal_hyp]
+        if op.type == op.remove:
+            #Since we have to remove the GT category, the text for the ref. node is wrong
+            marks_ref[idx_traversal_ref] = True
+
+            #Traverse the reference ne to find node
+            node_to_modify = ref_ne
+            father_node = ref_ne
+            for node_step in path_node_ref[1:]:
+                father_node = node_to_modify
+                node_to_modify = node_to_modify[CHILDREN][node_step[1]]
+            
+            #Delete child category by slicing the father
+            #Insert the text from the removed node into the father
+            slicing_point = path_node_ref[-1][1]
+            father_node[CHILDREN] = (father_node[CHILDREN][:slicing_point] +
+                                     node_to_modify[CHILDREN] +
+                                     father_node[CHILDREN][slicing_point+1:])
+
+            #Deletion of node (match op.arg1 -> None), advance in traversal for ref. tree
+            idx_traversal_ref += 1
+        elif op.type == op.insert:
+            #Since we have to insert the hypothesized category, the text for the hyp. node is wrong
+            marks_hyp[idx_traversal_hyp] = True
+
+            #An insertion in the reference tree is going to be treated as a deletion in hyp. tree
+            #Traverse the hyp ne to find node
+            node_to_modify = hyp_ne
+            father_node = hyp_ne
+            for node_step in path_node_hyp[1:]:
+                father_node = node_to_modify
+                node_to_modify = node_to_modify[CHILDREN][node_step[1]]
+            
+            #Delete child category by slicing the father
+            #Insert the text from the removed node into the father
+            slicing_point = path_node_hyp[-1][1]
+            father_node[CHILDREN] = (father_node[CHILDREN][:slicing_point] +
+                                     node_to_modify[CHILDREN] +
+                                     father_node[CHILDREN][slicing_point+1:])
+
+            #Insertion of node (match None -> op.arg2), advance in traversal for hyp. tree
+            idx_traversal_hyp += 1
+        elif op.type == op.update:
+            #Since we have to update the label, the text for both ref. and hyp. nodes is wrong
+            marks_ref[idx_traversal_ref] = True
+            marks_hyp[idx_traversal_hyp] = True
+
+            #Traverse the reference ne to find node
+            node_to_modify = ref_ne
+            for node_step in path_node_ref[1:]:
+                node_to_modify = node_to_modify[CHILDREN][node_step[1]]
+            
+            #Update the label in reference 
+            node_to_modify[CATEGORY] = str(Node.get_label(op.arg2))
+
+            #Label substitution, advance in the post-order traversal for both tagging trees 
+            idx_traversal_hyp += 1
+            idx_traversal_ref += 1
+        elif op.type == op.match:
+            #Perfect node match, advance in the post-order traversal for both tagging trees
+            idx_traversal_hyp += 1
+            idx_traversal_ref += 1
+        
+    
+    tagged_ref_transc = obtain_list_tagged_transcriptions(ref_ne, dict())
+    marked_ref_ne = add_marks_to_named_entity(orig_ref_ne, marks_ref)
+    marked_ref_transc = obtain_marked_transcriptions(marked_ref_ne)
+    
+    tagged_hyp_transc = obtain_list_tagged_transcriptions(hyp_ne, dict())
+    marked_hyp_ne = add_marks_to_named_entity(orig_hyp_ne, marks_hyp)
+    marked_hyp_transc = obtain_marked_transcriptions(marked_hyp_ne)
+
+    tagged_and_marked_ref_transc = []
+    for idx, tagged_item in enumerate(tagged_ref_transc):
+        marked_item = marked_ref_transc[idx]
+        tagged_and_marked_ref_transc.append((marked_item[0], tagged_item[1], tagged_item[0]))
+
+    tagged_and_marked_hyp_transc = []
+    for idx, tagged_item in enumerate(tagged_hyp_transc):
+        marked_item = marked_hyp_transc[idx]
+        tagged_and_marked_hyp_transc.append((marked_item[0], tagged_item[1], tagged_item[0]))
+
+    return (tagged_and_marked_ref_transc, tagged_and_marked_hyp_transc)
+    
+
+#Given 2 lists of tagged marked transcriptions, compute edit distance. Transcriptions incorporate the tag for each token
+#and a mark (boolean) to indicate whether the substitution cost for that token must always be one (True) or can be computed
+#normally (False). Therefore, each element is a tuple of three elements (MARKED: {True, False}, token: str, tag: str)
+#if it is necessary to compute char-level edit distance, the elements must be split previously
+def calc_edit_dist(ref_ne: list, hyp_ne: list) -> float:
+    #Exception: substituting empty NE with empty NE
+    if len(ref_ne) == 0 and len(hyp_ne) == 0:
+        raise Exception("Reference entity and hypothesized entity were of size 0")
+
+    #Substituting an empty NE with a normal NE, cost 1
+    if len(ref_ne) == 0 or len(hyp_ne) == 0:
+        return 1.0
+    
+    (ref_tagged_transcription, hyp_tagged_transcription) = obtain_tagged_and_marked_transcriptions(ref_ne, hyp_ne)
+
+    #Compute levenshtein distance considering marked elements and tags
+    LEN_VECTOR = len(ref_tagged_transcription)+1
+    prev_dist_vec = [0]*(LEN_VECTOR)
+    dist_vec = [0]*(LEN_VECTOR)
+
+    #Initialize previous vector
+    for i in range(LEN_VECTOR):
+        prev_dist_vec[i] = i
+
+    #Dynamic programming version of levenshtein distance
+    for j in range(1, len(hyp_tagged_transcription) + 1):
+        dist_vec[0] = j
+        for i in range(1, LEN_VECTOR):
+            dist_ins = 1 + dist_vec[i - 1]
+            dist_del = 1 + prev_dist_vec[i]
+            cost_sus = 0
+            if (ref_tagged_transcription[i - 1][0] == True #Reference marked as wrong
+                or hyp_tagged_transcription[j - 1][0] == True #Hypothesis marked as wrong
+                or ref_tagged_transcription[i - 1][2] != hyp_tagged_transcription[j - 1][2] #Tagging does not match
+                or ref_tagged_transcription[i - 1][1] != hyp_tagged_transcription[j - 1][1]): #Transcription does not match
+                cost_sus = 1
+            
+            dist_sus = prev_dist_vec[i - 1] + cost_sus
+
+            dist_vec[i] = min(dist_ins, dist_del, dist_sus)
+        
+        prev_dist_vec = copy.deepcopy(dist_vec)
+        dist_vec = [0] * (LEN_VECTOR)
+
+    #Return saturated edit distance
+    return min(1.0, float(prev_dist_vec[-1]) / len(ref_tagged_transcription))
---
+++ exhaustive_test.py
@@ -0,0 +1,227 @@
+from zss import Node
+from compute_nested_substitution_cost import calc_edit_dist, obtain_tagged_and_marked_transcriptions, post_order_traversal_named_entity
+
+# <A> a <B> b </B> c </A>
+g_0_ne = [
+    "A", [
+        "a",
+        [
+            "B", [
+                "b"
+            ]
+        ],
+        "c"
+    ]
+]
+
+# <persName> George Washington </persName>
+g_1_ne_word = [
+    "persName", [
+        "George",
+        "Washington"
+    ]
+]
+
+g_1_ne_char = [
+    "persName", [
+        "G",
+        "e",
+        "o",
+        "r",
+        "g",
+        "e",
+        " ",
+        "W",
+        "a",
+        "s",
+        "h",
+        "i",
+        "n",
+        "g",
+        "t",
+        "o",
+        "n"
+    ]
+]
+
+# <A> a b <C> c </C> d</A>
+g_2_ne = [
+    "A", [
+        "a",
+        "b",
+        [
+            "C", [
+                "c"
+            ]
+        ],
+        "d"
+    ]
+]
+
+# Empty NE
+h_0_ne = []
+
+# <A> a b c </A> => Case of deletion of a category <B>
+h_1_ne = [
+    "A", [
+        "a",
+        "b",
+        "c"
+    ]
+]
+
+# <A> a <B> b </B> </A> => Case of missing a part of transcription
+h_2_ne = [
+    "A", [
+        "a",
+        [
+            "B", [
+                "b"
+            ]
+        ]
+    ]
+]
+
+# <A> <B> b </B> c </A> => Case of missing a part of the transcription (in different part)
+h_3_ne = [
+    "A", [
+        [
+            "B", [
+                "b"
+            ]
+        ],
+        "c"
+    ]
+]
+
+# <A> e <B> f </B> g </A> => Transcription substitutions
+h_4_ne = [
+    "A", [
+        "e",
+        [
+            "B", [
+                "f"
+            ]
+        ],
+        "g"
+    ]
+]
+
+# <A> e <B></B> g </A> => Category without transcription (shouldn't be possible)
+h_5_ne = [
+    "A", [
+        "e",
+        [
+            "B", []
+        ],
+        "g"
+    ]
+]
+
+# <persName> George </persName>
+h_6_ne_word = [
+    "persName", [
+        "George"
+    ]
+]
+
+h_6_ne_char = [
+    "persName", [
+        "G",
+        "e",
+        "o",
+        "r",
+        "g",
+        "e",
+    ]
+]
+
+# <placeName> George </placeName>
+h_7_ne_word = [
+    "placeName", [
+        "George"
+    ]
+]
+
+h_7_ne_char = [
+    "placeName", [
+        "G",
+        "e",
+        "o",
+        "r",
+        "g",
+        "e",
+    ]
+]
+
+# <A> a <C> b <C> c</C></C> d</A>
+h_8_ne = [
+    "A", [
+        "a",
+        [
+            "C", [
+                "b", 
+                [
+                    "C", [
+                        "c"
+                    ]
+                ]
+            ]
+        ],
+        "d"
+    ]
+]
+
+# <A> a <C> b <D> c</D></C> d</A>
+h_9_ne = [
+    "A", [
+        "a",
+        [
+            "C", [
+                "b", 
+                [
+                    "D", [
+                        "c"
+                    ]
+                ]
+            ]
+        ],
+        "d"
+    ]
+]
+
+assert calc_edit_dist(g_0_ne, h_0_ne) == 1.0
+assert calc_edit_dist(h_0_ne, g_0_ne) == 1.0
+
+assert abs(0.33333333 - calc_edit_dist(g_0_ne, h_1_ne)) < 0.0001
+assert abs(0.33333333 - calc_edit_dist(h_1_ne, g_0_ne)) < 0.0001
+
+assert abs(0.33333333 - calc_edit_dist(g_0_ne, h_2_ne)) < 0.0001
+assert abs(0.5 - calc_edit_dist(h_2_ne, g_0_ne)) < 0.0001
+
+assert abs(0.33333333 - calc_edit_dist(g_0_ne, h_3_ne)) < 0.0001
+assert abs(0.5 - calc_edit_dist(h_3_ne, g_0_ne)) < 0.0001
+
+assert (calc_edit_dist(h_4_ne, g_0_ne)) == 1.0
+assert (calc_edit_dist(g_0_ne, h_4_ne)) == 1.0
+
+assert calc_edit_dist(g_0_ne, h_5_ne) == 1.0
+assert calc_edit_dist(h_5_ne, g_0_ne) == 1.0
+
+assert calc_edit_dist(g_1_ne_word, h_6_ne_word) == 0.5
+assert calc_edit_dist(h_6_ne_word, g_1_ne_word) == 1.0
+
+assert abs(0.647058824 - calc_edit_dist(g_1_ne_char, h_6_ne_char)) < 0.0001
+assert calc_edit_dist(h_6_ne_char, g_1_ne_char) == 1.0
+
+assert calc_edit_dist(g_1_ne_word, h_7_ne_word) == 1.0
+assert calc_edit_dist(h_7_ne_word, g_1_ne_word) == 1.0
+
+assert calc_edit_dist(g_1_ne_char, h_7_ne_char) == 1.0
+assert calc_edit_dist(h_7_ne_char, g_1_ne_char) == 1.0
+
+assert calc_edit_dist(g_2_ne, h_8_ne) == 0.25
+assert calc_edit_dist(h_8_ne, g_2_ne ) == 0.25
+
+assert calc_edit_dist(g_2_ne, h_9_ne) == 0.5
+assert calc_edit_dist(h_9_ne, g_2_ne ) == 0.5
